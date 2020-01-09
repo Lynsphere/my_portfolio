aeccb9be64385a2dfa06c675aa279a38f9e910d8
Lynsphere <58320586+Lynsphere@users.noreply.github.com>
2020-01-09 17:29:03 +0900

    Add files via upload
    미완성 1차 업로드

---
+++ js/main.js
@@ -0,0 +1,54 @@
+/* main.js */
+$(function(){
+    // gnb 색상변환
+    $('#home').mouseenter(function(){
+        $('#gnb').css('border-left','35px solid #898989')
+    });
+    $('#about').mouseenter(function(){
+        $('#gnb').css('border-left','35px solid #00aeef')
+    });
+    $('#portfolio').mouseenter(function(){
+        $('#gnb').css('border-left','35px solid #00a651')
+    });
+    $('#contact').mouseenter(function(){
+        $('#gnb').css('border-left','35px solid #ed145b')
+    });
+   
+    // gnb
+    $('#home').click(function(){
+        $('section').removeClass('show')
+        $('#s1').addClass('show')
+        $('#home_box').addClass('bounceInUp animated')
+    });
+    $('#home_box').on('animationend', function(){
+        $(this).removeClass('bounceInUp animated');
+      });
+
+    $('#about').click(function(){
+        $('section').removeClass('show')
+        $('#s2').addClass('show')
+        $('#about_box').addClass('bounceInUp animated')
+    });
+    $('#about_box').on('animationend', function(){
+        $(this).removeClass('bounceInUp animated');
+      });
+
+    $('#portfolio').click(function(){
+        $('section').removeClass('show')
+        $('#s3').addClass('show')
+        $('#portfolio_box').addClass('bounceInUp animated')
+    });
+    $('#portfolio_box').on('animationend', function(){
+        $(this).removeClass('bounceInUp animated');
+      });
+
+    $('#contact').click(function(){
+        $('section').removeClass('show')
+        $('#s4').addClass('show')
+        $('#contact_box').addClass('bounceInUp animated')
+    });
+    $('#contact_box').on('animationend', function(){
+        $(this).removeClass('bounceInUp animated');
+      });
+
+});
